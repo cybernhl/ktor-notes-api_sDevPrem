4a79bbaa3211a836dec65783920a155c5269dc96
Neo <c.min.tseng@gmail.com>
2026-01-29 01:51:30 +0800

    add libs.versions.toml for migrate build to version catalogs & upgrade kotlin to 2.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,10 @@
 val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
+
 val ktorm_version = "3.6.0"
 val koin_version = "3.4.1"
 
 plugins {
-    kotlin("jvm") version "1.8.22"
+    alias(libs.plugins.kotlin.jvm)
     id("io.ktor.plugin") version "2.3.1"
 }
 
@@ -40,9 +39,9 @@ dependencies {
     implementation("io.ktor:ktor-server-auth-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation(libs.logback.classic)
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation(libs.kotlin.test.junit)
 
     implementation("org.ktorm:ktorm-core:${ktorm_version}")
     implementation("org.ktorm:ktorm-support-mysql:${ktorm_version}")
